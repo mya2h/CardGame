e8c75a9685858ce13174fcff6fef69eb4cdaf2e8
kalkidan <mesfinkalkidan@gmail.com>
2021-02-20 20:39:16 +0300

    canvas game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import CardGame from './CardGame.js';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-body">
+      <CardGame> </CardGame>
+      </div>
+    </div>
+  );
+}
+
+export default App;
